4ecabe113386ad877890d418c24d11470e3279a9
Mrk0p <sachinwele23@gmail.com>
2023-01-17 13:42:55 +0530

    Added : Till won or loss 50% of a Stack logic

--- src/com/bridgelabz/gambling_simulator/GamblingSimulator.java
+++ src/com/bridgelabz/gambling_simulator/GamblingSimulator.java
@@ -6,26 +6,35 @@ public class GamblingSimulator {
 
     public static final int initialStake = 100;
     public static final int stakeBet = 1;
-    static Random random = new Random();
+    public static final float percentage = (initialStake / 100) * 50;
+    public static final float highestStake = percentage + initialStake;
+    public static final float lowestStake = percentage - initialStake;
 
     public static void main(String[] args) {
 
+        Random random = new Random();
         int totalStake = initialStake;
 
-        int play = random.nextInt(2);
+        while (totalStake < highestStake && totalStake > lowestStake) {
+            int play = random.nextInt(2);
 
-        switch (play) {
+            switch (play) {
 
-            case 0:
-                totalStake = totalStake - stakeBet;
-                System.out.println("Stake after loosing: " + totalStake);
-                break;
+                case 0:
+                    totalStake = totalStake - stakeBet;
+                    System.out.println("Stake after loosing: " + totalStake);
+                    break;
 
-            case 1:
-                totalStake = totalStake + stakeBet;
-                System.out.println("Stake after winning: " + totalStake);
-                break;
+                case 1:
+                    totalStake = totalStake + stakeBet;
+                    System.out.println("Stake after winning: " + totalStake);
+                    break;
+            }
+
         }
+        if (totalStake == highestStake)
+            System.out.println("Gambler won by: " + totalStake);
+        else
+            System.out.println("Gambler lost by: " + totalStake);
     }
-
 }
