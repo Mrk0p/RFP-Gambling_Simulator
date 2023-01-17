4fd8f59944b48a56f7a9e102b83fd46dd54deba5
Mrk0p <sachinwele23@gmail.com>
2023-01-17 13:33:42 +0530

    Added : Win or Loose logic

--- src/com/bridgelabz/gambling_simulator/GamblingSimulator.java
+++ src/com/bridgelabz/gambling_simulator/GamblingSimulator.java
@@ -1,15 +1,31 @@
 package com.bridgelabz.gambling_simulator;
 
+import java.util.Random;
+
 public class GamblingSimulator {
 
     public static final int initialStake = 100;
     public static final int stakeBet = 1;
+    static Random random = new Random();
 
     public static void main(String[] args) {
 
-        System.out.println("Initial Stake is: " + initialStake + "$");
-        System.out.println("Stake Bet is: " + stakeBet);
+        int totalStake = initialStake;
 
+        int play = random.nextInt(2);
+
+        switch (play) {
+
+            case 0:
+                totalStake = totalStake - stakeBet;
+                System.out.println("Stake after loosing: " + totalStake);
+                break;
+
+            case 1:
+                totalStake = totalStake + stakeBet;
+                System.out.println("Stake after winning: " + totalStake);
+                break;
+        }
     }
 
 }
